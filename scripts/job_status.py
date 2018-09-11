f69dd1be47d199e43da2df35bb04cf8d72bcbea9
Cameron Kuchta <kuchtact@beloit.edu>
2018-09-11 12:12:50 -0400

    Get status finding working (hopefully)

--- scripts/job_status.py
+++ scripts/job_status.py
@@ -32,29 +32,27 @@ class Job(object):
         self.jobId = j.jobId
 
         # Get the status of the job.
-        if j.status:
-            print("jobId:", self.jobId, "RUN:", lsf.JOB_STAT_RUN, "DONE:", lsf.JOB_STAT_DONE,"EXIT:", lsf.JOB_STAT_EXIT, "USUSP:", lsf.JOB_STAT_USUSP, "SSUSP:", lsf.JOB_STAT_SSUSP, "PEND:", lsf.JOB_STAT_PEND)
-            if lsf.JOB_STAT_RUN:
-                self.status = "Running"
-            elif lsf.JOB_STAT_DONE:
-                self.status = "Complete"
-            elif lsf.JOB_STAT_EXIT:
-                self.status = "Killed"
-            elif lsf.JOB_STAT_USUSP:
-                self.status = "Susp_person"
-            elif lsf.JOB_STAT_SSUSP:
-                self.status = "Susp_system"
-            elif lsf.JOB_STAT_PEND:
-                if j.pendStateJ == 0:
-                    self.status = "Eligible"
-                else:
-                    self.status = "Blocked_system"
-            elif lsf.JOB_STATE_PEND:
-                self.status = "Blocked_person"
+        # print("jobId:", self.jobId, "RUN:", lsf.JOB_STAT_RUN, "DONE:", lsf.JOB_STAT_DONE,"EXIT:", lsf.JOB_STAT_EXIT, "USUSP:", lsf.JOB_STAT_USUSP, "SSUSP:", lsf.JOB_STAT_SSUSP, "PEND:", lsf.JOB_STAT_PEND)
+        if j.status & lsf.JOB_STAT_RUN:
+            self.status = "Running"
+        elif j.status & lsf.JOB_STAT_DONE:
+            self.status = "Complete"
+        elif j.status & lsf.JOB_STAT_EXIT:
+            self.status = "Killed"
+        elif j.status & lsf.JOB_STAT_USUSP:
+            self.status = "Susp_person"
+        elif j.status & lsf.JOB_STAT_SSUSP:
+            self.status = "Susp_system"
+        elif j.status & lsf.JOB_STAT_PEND:
+            if j.pendStateJ == 0:
+                self.status = "Eligible"
             else:
-                self.status = "Unkown"
+                self.status = "Blocked_system"
+        elif j.status & lsf.JOB_STATE_PEND:
+            self.status = "Blocked_person"
         else:
             self.status = "Unknown"
+        print("jobId:", self.jobId, "status:", self.status)
 
 
 if __name__ == "__main__":
