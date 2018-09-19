23706ba69dc112105404d3aa2913ffd81eaa416b
Cameron Kuchta <kuchtact@beloit.edu>
2018-09-19 09:58:49 -0400

    Cleanup test_status.

--- scripts/test_status.py
+++ scripts/test_status.py
@@ -3,6 +3,7 @@ from scripts import parse_file
 from scripts.notifications.slack_app import slack_send
 import os
 import sys
+import time
 
 def check_tests(rgt_in_path):
     rgt_parser = parse_file.ParseRGTInput()
@@ -11,7 +12,8 @@ def check_tests(rgt_in_path):
 
     error_not_queued = "Tests not currently in queue or running:\n"
     error_not_exist = "Tests that do not exist:\n"
-    problems = False
+    queued_problems = False
+    exist_problems = False
     jobID_parser = parse_file.ParseJobID()
     for i in range(len(test_directories)):
         d = test_directories[i]
@@ -20,16 +22,23 @@ def check_tests(rgt_in_path):
             id = jobID_parser.parse_file(jobID_path)
         except FileNotFoundError:
             error_not_exist += "\t" + test_list[i]['program'] + "\t" + test_list[i]['test'] + "\n"
-            problems = True
+            exist_problems = True
             continue
 
         if not in_queue(id):
             error_not_queued += "\t" + test_list[i]['program'] + "\t" + test_list[i]['test'] + "\n"
-            problems = True
+            queued_problems = True
 
-    error_str = error_not_exist + "\n\n" + error_not_queued
-    if problems:
-        slack_send.send_message(error_str)
+    error_str = ""
+    if exist_problems:
+        error_str += error_not_exist + "\n"
+    if queued_problems:
+        error_str += error_not_queued
+
+    if exist_problems or queued_problems:
+        app = slack_send.SlackApp(os.environ["SLACK_BOT_TOKEN"])
+        channel = "CCRA1Q41J"
+        app.send_message(channel=channel, message=error_str)
 
 
 def get_test_directories(tests_path, test_list):
@@ -51,7 +60,12 @@ def in_queue(jobid):
 
 
 if __name__ == '__main__':
-    rgt_in_path = sys.argv[1]
+    try:
+        rgt_in_path = sys.argv[1]
+    except IndexError:
+        raise IndexError("No rgt.input file path set.")
+
     while True:
+        print("Sending message . . .")
         check_tests(rgt_in_path)
-        os.sleep(300)
+        time.sleep(300)
