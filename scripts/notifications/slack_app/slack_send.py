96440b0478c77b7b97048d142153558dced9d969
Cameron Kuchta <kuchtact@beloit.edu>
2018-09-26 14:12:59 -0400

    Change git ignore to ignore pyc files. Can read message from slack.

--- scripts/notifications/slack_app/slack_send.py
+++ scripts/notifications/slack_app/slack_send.py
@@ -1,5 +1,6 @@
 from slackclient import SlackClient
 import os
+import time
 
 
 class SlackApp:
@@ -37,22 +38,54 @@ class SlackApp:
         else:
             raise Exception("Connection failed. Exception traceback printed above.")
 
-    def search_message(self):
+    def search_messages(self, message):
+        start = time.time()
         if self.client.rtm_connect(with_team_state=False):
-            self.client.api_call(
+            response = self.client.api_call(
                 "search.messages",
-                query="export"
+                query=message,
+                sort='timestamp'
             )
-            print("I did it!")
         else:
             raise Exception("Connection failed. Exception traceback printed above.")
 
+        if not response['ok']:
+            raise Exception(str(response))
+
+        print(time.time() - start)
+        # Print response
+        print(print_dic(response))
+
+        print("Number of messages:",len(response['messages']['matches']))
+
+        return response
+
+    def get_time_stamp(self, response_message):
+        exit()
+
+def print_dic(dic, indent=0):
+    message = ""
+    tab = "    "
+    new_line = '\n'
+    if type(dic) is dict:
+        for key in dic.keys():
+            message += new_line + indent*tab + "'" + key + "': {" + print_dic(dic[key], indent=indent+1) + "}" + new_line + indent*tab
+    elif type(dic) is list:
+        for element in dic:
+            message += new_line + indent*tab + "[" + print_dic(element, indent=indent+1) + "]" + new_line + indent*tab
+    else:
+        message += tab + str(dic) + tab
+        message = message.encode('ascii', 'replace')
+        message = message.decode('ascii')
+    return message
+
+# Only respond to messages if they are somewhat recent. How to look for mentions? Reduce the number of 'responses' since duplicated.
 
 if __name__ == '__main__':
     # Pull the bot token from the os environment and create the connector.
-    app = SlackApp(os.environ["SLACK_BOT_TOKEN"])
-    app.search_message()
+    app = SlackApp(os.environ["SLACK_USER_TOKEN"])
+    app.search_messages("example_example_example_example_example")
     # # This is the id of the channel.
     # channel = "CCRA1Q41J"
     # # Send the message to the channel.
-    # app.send_message(channel=channel, message="Another test message. This time from summitdev!")
+    # app.send_message(channel=channel, message="BWAHAHAHHAHAHAHAHA!!!!! The computer has taken Cameron's account!!! The slack client needs user permissions and thus needs it's own account.")
