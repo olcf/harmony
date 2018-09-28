5801be937460ddc1acb6573d9956594005b649ff
Reuben D. Budiardja <reubendb@ornl.gov>
2018-09-28 13:47:03 -0400

    Draft of DB schema (via its creation script).

---
+++ db/create.sql
@@ -0,0 +1,60 @@
+
+use harmony
+
+CREATE TABLE IF NOT EXISTS `rgt_event`
+  (
+  `event_id`          INT (6)         NOT NULL,
+  `event_uid`         SMALLINT        NOT NULL,
+  `event_name`        VARCHAR (256)   NOT NULL,
+  `timestamp`         TIMESTAMP       ,
+  PRIMARY KEY ( `event_id` ),
+  UNIQUE  KEY `event_uid`  ( `event_uid` )
+  );
+
+
+CREATE TABLE IF NOT EXISTS `rgt_check`
+  (
+  `check_id`          INT (6)         NOT NULL,
+  `check_uid`         TINYINT         NOT NULL,
+  `check_desc`        VARCHAR (1024)  NOT NULL,
+  `timestamp`         TIMESTAMP       ,
+  PRIMARY KEY ( `check_id` ),
+  UNIQUE KEY  `check_uid` ( `check_uid` )
+  );
+
+CREATE TABLE IF NOT EXISTS `rgt_test`
+  (
+  `test_id`           INT (6)         NOT NULL,
+  `harness_uid`       CHAR (36)       NOT NULL,
+  `harness_start`     DATETIME        NOT NULL,
+  `harness_tld`       VARCHAR (1024)  NOT NULL,
+  `application`       VARCHAR (1024)  NOT NULL,
+  `testname`          VARCHAR (1024)  NOT NULL,
+  `job_id`            CHAR (64)       NULL,
+  `job_status`        TINYINT         NULL,
+  `build_status`      TINYINT         NULL,
+  `submit_status`     TINYINT         NULL,
+  `check_status`      TINYINT         NULL,
+  `output_build`      TEXT            NULL,
+  `output_submit`     TEXT            NULL,
+  `output_check`      TEXT            NULL,
+  `output_report`     TEXT            NULL,
+  `system`            VARCHAR (64)    NOT NULL,
+  `timestamp`         TIMESTAMP       ,
+  PRIMARY KEY  ( `test_id` ),
+  UNIQUE  KEY `harness_uid`  ( `harness_uid` ),
+  FOREIGN KEY ( `check_status` ) REFERENCES `rgt_check` ( `check_uid` )
+  );
+
+CREATE TABLE IF NOT EXISTS `rgt_test_event`
+  (
+  `id`                INT (6)         NOT NULL,
+  `test_id`           INT (6)         NOT NULL,
+  `event_id`          INT (6)         NOT NULL,
+  `event_time`        DATETIME        NOT NULL,
+  `timestamp`         TIMESTAMP       ,
+  PRIMARY KEY ( `id` ),
+  FOREIGN KEY ( `test_id` ) REFERENCES `rgt_test` ( `test_id` ),
+  FOREIGN KEY ( `event_id` ) REFERENCES `rgt_event` ( `event_id` ),
+  UNIQUE  KEY `test_event` ( `test_id`, `event_id` )
+  );
